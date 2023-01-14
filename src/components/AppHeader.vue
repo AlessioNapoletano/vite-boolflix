3aa47c2065066e49871279935f7c221afea4f1f5
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-14 04:18:29 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Questo è l'header
+    </h1>
+  </header>
+
+  <hr>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <main>
+        <h1>
+            Questo è il main
+        </h1>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
