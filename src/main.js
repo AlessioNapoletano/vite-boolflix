21b23cf71f52bebdf62c9f83fc2aa9e29e0f7f8f
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-16 16:17:17 +0100

    not work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import LangFlag from 'vue-lang-code-flags';
+
+Vue.component('lang-flag', LangFlag);
 
 createApp(App).mount('#app')
